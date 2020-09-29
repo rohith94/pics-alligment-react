13a458bd39b03e0d01b5762264f9c8920191aab6
Rohith M <rohith.m94@gmail.com>
2020-09-29 19:42:35 +0530

    passing data back to parent ffrom props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SearchBar from "./components/SearchBar/SearchBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  onSearchSubmit = (term) => {
+    console.log(term);
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+
+  state = { term: "" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // console.log(this.state.term);
+
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Image Search</label>
+            {/* <input type="text" onChange={this.onInputChange}></input> */}
+            <input
+              type="text"
+              onChange={(e) => {
+                this.setState({ term: e.target.value });
+              }}
+              value={this.state.term}
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
